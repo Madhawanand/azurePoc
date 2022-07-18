f893acb66a746771573a153d8fb88961fb9049a7
Madhawanand <madhavanand123@gmail.com>
2022-07-18 19:03:58 +0530

    Add files via upload

---
+++ BusinessLogic/poc.py
@@ -0,0 +1,20 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.edge.service import Service
+
+class Poc():
+    def invoke(self):
+     self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+     self.driver.get("https://www.youtube.com/")
+     self.driver.maximize_window()
+     self.driver.implicitly_wait(30)
+     self.driver.find_element(By.XPATH ,"//input[@id='search']").send_keys("Paramatma")
+     self.driver.close()
+ic= Poc()
+ic.invoke()
+
+
+
