a6b9f45125a59e1232fe6a050784e557d1b54a30
Madhawanand <madhavanand123@gmail.com>
2022-07-19 16:48:42 +0530

    Update poc.py

--- BusinessLogic/poc.py
+++ BusinessLogic/poc.py
@@ -4,17 +4,12 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.edge.service import Service
+class TestPoc:
 
-class Poc():
-    def invoke(self):
+    def test_invoke(self):
      self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
      self.driver.get("https://www.youtube.com/")
      self.driver.maximize_window()
      self.driver.implicitly_wait(30)
      self.driver.find_element(By.XPATH ,"//input[@id='search']").send_keys("Paramatma")
      self.driver.close()
-ic= Poc()
-ic.invoke()
-
-
-
