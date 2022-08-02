c516784b644948c4266055c3f609407503c35ab9
madhawanandsindigar <Msindigar@mycervello.om>
2022-08-02 07:14:16 +0530

    different interpreter

--- BusinessLogic/test_poc.py
+++ BusinessLogic/test_poc.py
@@ -1,7 +1,6 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.edge.service import Service
 
