02c1475f07cf22154c651858bc7ec36ccbd7bf2a
madhawanandsindigar <Msindigar@mycervello.om>
2022-08-02 06:47:19 +0530

    working code

---
+++ BusinessLogic/test_poc.py
@@ -0,0 +1,15 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.edge.service import Service
+
+
+class TestPoc:
+
+    def test_invoke(self):
+        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+        self.driver.get("https://www.youtube.com/")
+        self.driver.maximize_window()
+        self.driver.close()
