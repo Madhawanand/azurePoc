aa43f416d908697e9d4bb099df0d6525caa1e274
madhawanandsindigar <Msindigar@mycervello.om>
2022-08-03 19:51:59 +0530

    change in the Poc file

--- BusinessLogic/test_poc.py
+++ BusinessLogic/test_poc.py
@@ -7,7 +7,12 @@ from webdriver_manager.chrome import ChromeDriverManager
 class TestPoc:
 
     def test_invoke(self):
-        self.driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument('--no-sandbox')
+    #    chrome_options.add_argument('--window-size=1420,1080')
+    #    chrome_options.add_argument('--headless')
+    #    chrome_options.add_argument('--disable-gpu')
+        self.driver = webdriver.Chrome(chrome_options=chrome_options)
         self.driver.get("https://www.youtube.com/")
         self.driver.maximize_window()
         print(self.driver.current_url)
