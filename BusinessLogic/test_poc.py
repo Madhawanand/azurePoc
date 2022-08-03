588b4cdd93f20e01d0b97f20abaa33f7fe3f7288
madhawanandsindigar <Msindigar@mycervello.om>
2022-08-03 20:02:34 +0530

    with options

--- BusinessLogic/test_poc.py
+++ BusinessLogic/test_poc.py
@@ -7,12 +7,13 @@ from webdriver_manager.chrome import ChromeDriverManager
 class TestPoc:
 
     def test_invoke(self):
-        chrome_options = webdriver.ChromeOptions()
-        chrome_options.add_argument('--no-sandbox')
-    #    chrome_options.add_argument('--window-size=1420,1080')
-    #    chrome_options.add_argument('--headless')
-    #    chrome_options.add_argument('--disable-gpu')
-        self.driver = webdriver.Chrome(chrome_options=chrome_options)
+        options = webdriver.ChromeOptions()
+    #    options.add_argument('headless')
+        options.add_argument('--disable-infobars')
+        options.add_argument('--disable-dev-shm-usage')
+        options.add_argument('--no-sandbox')
+        options.add_argument('--remote-debugging-port=9222')
+        self.driver = webdriver.Chrome(options=options)
         self.driver.get("https://www.youtube.com/")
         self.driver.maximize_window()
         print(self.driver.current_url)
