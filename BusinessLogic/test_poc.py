5955f6815f5583ce87256edfa18c29a6acd3315d
madhawanandsindigar <Msindigar@mycervello.om>
2022-08-03 11:28:32 +0530

    change in the Poc file

--- BusinessLogic/test_poc.py
+++ BusinessLogic/test_poc.py
@@ -2,10 +2,13 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
+
+
 class TestPoc:
 
     def test_invoke(self):
         self.driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
         self.driver.get("https://www.youtube.com/")
         self.driver.maximize_window()
+        print(self.driver.current_url)
         self.driver.close()
