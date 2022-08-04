bc3af41ae8f61d37db851ee2ed90df938fd8b7bf
madhawanandsindigar <Msindigar@mycervello.om>
2022-08-04 10:16:37 +0530

    with Os changes

--- BusinessLogic/test_poc.py
+++ BusinessLogic/test_poc.py
@@ -1,6 +1,7 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
 
@@ -11,4 +12,7 @@ class TestPoc:
         self.driver.get("https://www.youtube.com/")
         self.driver.maximize_window()
         print(self.driver.current_url)
+
+    def test_search(self):
+        self.driver.find_element(By.XPATH, "//input[@id='search']").send_keys("paramatma")
         self.driver.close()
